1d7076f642adfcdb78e207522b894a9c30b3b4d7
Shin Kinoshita <shin.kinoshita@tcs.com>
2024-11-01 15:13:31 +0900

    fix staticDirs of storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,6 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  staticDirs: ["..\\public"],
+  staticDirs: [process.platform === "win32" ? "..\\public" : "../public"],
 };
 export default config;
